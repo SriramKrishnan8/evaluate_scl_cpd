2dd836aa621b4a928cd5892444d87cbf76fd71dc
Sriram Krishnan <sriramk8@gmail.com>
2025-07-22 19:00:22 +0530

    Produced output, error, result and gold-pred pairs; added fine and coarse grain evaluation; handled optional compound analysis parameter; added code for sample testing; updated gold compound codes

--- evaluate.py
+++ evaluate.py
@@ -11,8 +11,36 @@ from tqdm import tqdm
 import devtrans
 
 
-cgi_file = "/usr/lib/cgi-bin/scl_cpd/MT/anusaaraka.cgi"
+cgi_file = "/usr/lib/cgi-bin/scl/MT/anusaaraka.cgi"
 
+
+fine_to_coarse_dev = {
+    "अव्ययीभावः" : "अव्ययीभावः",
+    "इतरेतर-द्वन्द्वः" : "द्वन्द्वः",
+    "उपपद-तत्पुरुषः" : "तत्पुरुषः",
+    "कर्मधारयः" : "तत्पुरुषः",
+    "केवल-समासः" : "तत्पुरुषः",
+    "चतुर्थी-तत्पुरुषः" : "तत्पुरुषः",
+    "तत्पुरुषः" : "तत्पुरुषः",
+    "तृतीया-तत्पुरुषः" : "तत्पुरुषः",
+    "द्वन्द्वः" : "द्वन्द्वः",
+    "द्वितीया-तत्पुरुषः" : "तत्पुरुषः",
+    "नञ्-तत्पुरुषः" : "तत्पुरुषः",
+    "नञ्-बहुव्रीहिः" : "बहुव्रीहिः",
+    "पञ्चमी-तत्पुरुषः" : "तत्पुरुषः",
+    "प्रादि-तत्पुरुषः" : "तत्पुरुषः",
+    "बहुव्रीहिः" : "बहुव्रीहिः",
+    "षष्ठी-तत्पुरुषः" : "तत्पुरुषः",
+    "सप्तमी-तत्पुरुषः" : "तत्पुरुषः",
+    "उपमानम्" : "तत्पुरुषः",
+    "द्विगु-तत्पुरुषः" : "तत्पुरुषः",
+    "कर्मधारयः-1" : "कर्मधारयः",
+    "कर्मधारयः-2" : "कर्मधारयः",
+    "कर्मधारयः-3" : "कर्मधारयः",
+    "कर्मधारयः-4" : "कर्मधारयः",
+    "कर्मधारयः-5" : "कर्मधारयः",
+    "-" : "तत्पुरुषः",
+}
 
 cpd_codes = {
     "नञ्_त_पु" : "नञ्-तत्पुरुषः",
@@ -27,20 +55,143 @@ cpd_codes = {
     "त_पु_द्विगु" : "द्विगु-तत्पुरुषः",
     "उ_प_त_पु" : "उपपद-तत्पुरुषः",
     "क_धा" : "कर्मधारयः",
+    "क_धा_1" : "कर्मधारयः_1",
+    "क_धा_2" : "कर्मधारयः_2",
+    "क_धा_3" : "कर्मधारयः_3",
+    "क_धा_4" : "कर्मधारयः_4",
+    "क_धा_5" : "कर्मधारयः_5",
+    "ब_व्री" : "बहुव्रीहिः",
+    "अ_भा_1" : "अव्ययीभावः_1",
+    "अ_भा_2" : "अव्ययीभावः_2",
+    "अ_भा_3" : "अव्ययीभावः_3",
+    "अ_भा_4" : "अव्ययीभावः_4",
+    "अ_भा_5" : "अव्ययीभावः_5",
+    "अ_भा_6" : "अव्ययीभावः_6",
+    "अ_भा_7" : "अव्ययीभावः_7",
+    "द्वन्द्वः" : "इतरेतर-द्वन्द्वः",
+    "-" : "तत्पुरुषः",
+}
+
+cpd_codes_2 = {
+    "नञ्_त_पु" : "नञ्-तत्पुरुषः",
+    "त_पु_1" : "प्रथमा-तत्पुरुषः",
+    "त_पु_2" : "द्वितीया-तत्पुरुषः",
+    "त_पु_3" : "तृतीया-तत्पुरुषः",
+    "त_पु_4" : "चतुर्थी-तत्पुरुषः",
+    "त_पु_5" : "पञ्चमी-तत्पुरुषः",
+    "त_पु_6" : "षष्ठी-तत्पुरुषः",
+    "त_पु_7" : "सप्तमी-तत्पुरुषः",
+    "त_पु" : "तत्पुरुषः",
+    "त_पु_द्विगु" : "द्विगु-तत्पुरुषः",
+    "उ_प_त_पु" : "उपपद-तत्पुरुषः",
+    "क_धा" : "कर्मधारयः",
+    "ब_व्री" : "बहुव्रीहिः",
+    "अव्ययीभावः" : "अव्ययीभावः",
+    "अ_भा_1" : "अव्ययीभावः",
+    "अ_भा_2" : "अव्ययीभावः",
+    "अ_भा_3" : "अव्ययीभावः",
+    "अ_भा_4" : "अव्ययीभावः",
+    "अ_भा_5" : "अव्ययीभावः",
+    "अ_भा_6" : "अव्ययीभावः",
+    "अ_भा_7" : "अव्ययीभावः",
     "क_धा_1" : "कर्मधारयः",
     "क_धा_2" : "कर्मधारयः",
     "क_धा_3" : "कर्मधारयः",
     "क_धा_4" : "कर्मधारयः",
     "क_धा_5" : "कर्मधारयः",
-    "ब_व्री" : "बहुव्रीहिः",
-    "अ_भा" : "अव्ययीभावः",
-    "द्वन्द्वः" : "इतरेत-द्वन्द्वः",
+    "द्वन्द्वः" : "द्वन्द्वः",
+    "इतरेतर-द्वन्द्वः" : "द्वन्द्वः",
+    "अव्ययीभावः_1" : "अव्ययीभावः",
+    "अव्ययीभावः_2" : "अव्ययीभावः",
+    "अव्ययीभावः_3" : "अव्ययीभावः",
+    "अव्ययीभावः_4" : "अव्ययीभावः",
+    "अव्ययीभावः_5" : "अव्ययीभावः",
+    "अव्ययीभावः_6" : "अव्ययीभावः",
+    "अव्ययीभावः_7" : "अव्ययीभावः",
+    "कर्मधारयः_1" : "कर्मधारयः",
+    "कर्मधारयः_2" : "कर्मधारयः",
+    "कर्मधारयः_3" : "कर्मधारयः",
+    "कर्मधारयः_4" : "कर्मधारयः",
+    "कर्मधारयः_5" : "कर्मधारयः",
+    "अव्ययीभावः-1" : "अव्ययीभावः",
+    "अव्ययीभावः-2" : "अव्ययीभावः",
+    "अव्ययीभावः-3" : "अव्ययीभावः",
+    "अव्ययीभावः-4" : "अव्ययीभावः",
+    "अव्ययीभावः-5" : "अव्ययीभावः",
+    "अव्ययीभावः-6" : "अव्ययीभावः",
+    "अव्ययीभावः-7" : "अव्ययीभावः",
+    "कर्मधारयः-1" : "कर्मधारयः",
+    "कर्मधारयः-2" : "कर्मधारयः",
+    "कर्मधारयः-3" : "कर्मधारयः",
+    "कर्मधारयः-4" : "कर्मधारयः",
+    "कर्मधारयः-5" : "कर्मधारयः",
+    "-" : "तत्पुरुषः",
 }
+
+gold_map = {
+    "बहुव्रीहिः" : "बहुव्रीहिः",
+    "षष्ठी-तत्पुरुषः" : "षष्ठी-तत्पुरुषः",
+    "नञ्-तत्पुरुषः" : "नञ्-तत्पुरुषः",
+    "कर्मधारयः" : "कर्मधारयः",
+    "इतरेतर-द्वन्द्वः" : "द्वन्द्वः",
+    "उपपद-तत्पुरुषः" : "उपपद-तत्पुरुषः",
+    "तृतीया-तत्पुरुषः" : "तृतीया-तत्पुरुषः",
+    "सप्तमी-तत्पुरुषः" : "सप्तमी-तत्पुरुषः",
+    "द्वन्द्वः" : "द्वन्द्वः",
+    "अव्ययीभावः" : "अव्ययीभावः",
+    "पञ्चमी-तत्पुरुषः" : "पञ्चमी-तत्पुरुषः",
+    "नञ्_तत्पुरुषः" : "नञ्-तत्पुरुषः",
+    "नञ्‌-तत्पुरुषः" : "नञ्-तत्पुरुषः",
+    "नञ्-तत्पुरुष" : "नञ्-तत्पुरुषः",
+    "तत्पुरुषः" : "तत्पुरुषः",
+    "उपपद_तत्पुरुषः" : "उपपद-तत्पुरुषः",
+    "षष्ठी_तत्पुरुषः" : "षष्ठी-तत्पुरुषः",
+    "प्रादि-तत्पुरुषः" : "तत्पुरुषः",
+    "द्वन्दः" : "द्वन्द्वः",
+    "चतुर्थी-तत्पुरुषः" : "चतुर्थी-तत्पुरुषः",
+    "द्वितीया-तत्पुरुषः" : "द्वितीया-तत्पुरुषः",
+    "समाहार-द्वन्द्वः" : "द्वन्द्वः",
+    "नञ्-बहुव्रीहिः" : "नञ्-बहुव्रीहिः",
+    "सप्तमी_तत्पुरुषः" : "सप्तमी-तत्पुरुषः",
+    "षष्ठी-तत्पुरुष" : "षष्ठी-तत्पुरुषः",
+    "तृतीया_तत्पुरुषः" : "तृतीया-तत्पुरुषः",
+    "इतरेतर_द्वन्द्वः" : "द्वन्द्वः",
+    "षष्ठी-तत्पुरुषः-" : "षष्ठी-तत्पुरुषः",
+    "केवल-समासः" : "केवल-समासः",
+    "उपपद-तत्पुरुष" : "उपपद-तत्पुरुषः",
+    "इतरेतर-द्वन्दः" : "द्वन्द्वः",
+    "षष्ठी-सम्बन्धः" : "षष्ठी-तत्पुरुषः",
+    "प्रादि-समासः" : "तत्पुरुषः",
+    "द्वितीया_तत्पुरुषः" : "द्वितीया-तत्पुरुषः",
+    "कर्मधारय" : "कर्मधारयः",
+    "उपपदसमासः" : "उपपद-तत्पुरुषः",
+    "उपपद-समासः" : "उपपद-तत्पुरुषः",
+    "सुप्-समुच्चितः" : "द्वन्द्वः",
+    "सम्बन्धः" : "तत्पुरुषः",
+    "प्रादि_तत्पुरुषः" : "तत्पुरुषः",
+    "पञ्चमी_तत्पुरुषः" : "पञ्चमी-तत्पुरुषः",
+    "पञ्च-तत्पुरुषः" : "पञ्चमी-तत्पुरुषः",
+    "नञ्_बहुव्रीहिः" : "नञ्-बहुव्रीहिः",
+    "द्विगु-तत्पुरुषः" : "द्विगु-तत्पुरुषः",
+    "तॄतीया-तत्पुरुषः" : "तृतीया-तत्पुरुषः",
+    "चतुर्थी_तत्पुरुषः" : "चतुर्थी-तत्पुरुषः",
+    "केवल_समासः" : "केवल-समासः",
+    "कर्मप्रवचनीयः" : "अव्ययीभावः",
+    "इतरेतरद्वन्द्वः" : "द्वन्द्वः",
+    "नञ्-तत्पुरुष" : "नञ्-तत्पुरुषः",
+}
+
 
 def get_code_for_relation(relation):
     """ """
     
     return cpd_codes.get(relation, relation)
+    
+
+def get_code_for_relation_2(relation):
+    """ """
+    
+    return cpd_codes_2.get(relation, relation)
 
 
 def read_contents(name_):
@@ -52,47 +203,11 @@ def read_contents(name_):
     
     file_lines = list(filter(None, file_contents.split("\n")))
     
-    return file_lines[1:]
-
-
-def get_cpds(lines):
-    """ """
-    
-    cpds = []
-    for line in lines:
-        items = line.split("\t")
-        index = items[0]
-        word = items[1]
-        kAraka_sambanXa = items[6]
-        
-        if "-" in word or "_" in word:
-            cpds.append(word, kAraka_sambanXa)
-            
-    return cpds
-
-
-def compare_with_gold(gold_tsv, pred_tsv):
-    """ """
-    
-    gold_lines = read_contents(gold_tsv)
-    pred_lines = read_contents(pred_tsv)
-    
-    p = 0
-    for g in gold_lines:
-        g_items = gold_lines[g].split("\t")
-        g_index = g_items[0]
-        g_word = g_items[1]
-        
-        p_items = pred_lines[p].split("\t")
-        p_index = p_items[0]
-        p_word = p_items[1]
-        
-#        if p_word == g_word:
-#            
+    return file_lines[1:]        
     
 
 def call_anusaaraka(encoding, splitter, out_encoding, parse,
-                    text_type, tlang, mode, text):
+                    text_type, tlang, mode, text, cpd_analysis):
     """ """
     
 #    out_enc = output_encoding if output_encoding in ["roma", "deva"] else "roma"
@@ -105,6 +220,7 @@ def call_anusaaraka(encoding, splitter, out_encoding, parse,
         "text_type=" + text_type,#.replace(" ", "+"),
         "tlang=" + tlang,
         "mode=" + mode,
+        "compound_analysis=" + cpd_analysis,
         "text=" + text,
     ]
     
@@ -112,20 +228,23 @@ def call_anusaaraka(encoding, splitter, out_encoding, parse,
     command = query_string + " " + cgi_file
     
     original_cwd = os.getcwd()
-    os.chdir("/usr/lib/cgi-bin/scl_cpd/MT")
+    os.chdir("/usr/lib/cgi-bin/scl/MT")
     
-#    print(command)
+    print(command)
     
     try:
         p = sp.Popen(command, stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
         outs, errs = p.communicate()
     except Exception as e:
         result = ""
-        status = "Failure: " + str(e)
+        status = "Failed: " + str(e)
+        # print("Exception: " + status)
     else:
         result = outs.decode('utf-8')
         status = "Success"
         error = errs.decode('utf-8')
+        # print("Result: " + result)
+        # print("Error: " + error)
     finally:
         # Restore the original working directory
         os.chdir(original_cwd)
@@ -143,13 +262,16 @@ def get_compound_annotation(dep_res_json_str):
         words_analysis = sent_json["sent"]
         word_rel = []
         for word_json in words_analysis:
-            index = word_json["index"]
+            index = word_json["anvaya_no"]
             word = word_json["word"]
             rel = word_json["kaaraka_sambandha"]
             if ";" in rel:
                 rel = rel.split(";")[0]
+            if "#" in rel:
+                rel = rel.split("#")[0]
             if "," in rel:
                 rel_type, rel_index = rel.split(",")
+                rel_type = rel_type.replace("_", "-")
                 rel = f"{get_code_for_relation(rel_type)},{rel_index}"
             word_rel.append((index, word, rel))
         
@@ -168,6 +290,9 @@ def handle_xvanxva(annotated_sentence):
     index_changes = {}
     for index, word, rel in annotated_sentence:
         new_index = ""
+        if not rel or rel == "-" or "," not in rel:
+            rel = "-,-"
+        
         if "_" in word:
             d_cmpnts = word.split("_")
             w = 0
@@ -197,8 +322,10 @@ def handle_xvanxva(annotated_sentence):
             new_details.append((index, new_index, word, rel, "old"))
             is_cpd = True
             cur_cpd_index += 1
+            # For multiple relations
+            if "#" in rel:
+                rel = rel.split("#")[0]
             rel_type, rel_index = rel.split(",")
-            rel_w_index, rel_c_index = rel_index.split(".")
         elif is_cpd and "-" not in word:
             new_index = f"{str(cur_word_index)}.{str(cur_cpd_index)}"
             new_details.append((index, new_index, word, rel, "old"))
@@ -219,6 +346,8 @@ def handle_xvanxva(annotated_sentence):
         if not rel or rel == "-":
             new_details_updated.append((new_index, word, rel))
             break
+        if "#" in rel:
+            rel = rel.split("#")[0]
         rel_type, rel_index = rel.split(",")
         if rel_index in index_changes and updated == "old":
             new_rel = f"{rel_type},{index_changes[rel_index]}"
@@ -238,22 +367,28 @@ def test(sentence, encoding):
     text_type = "Sloka"
     tlang = "Hindi"
     mode = "json"
+    cpd_analysis = "YES"
     
     res, status, error = call_anusaaraka(
         encoding, splitter, out_encoding, 
-        parse, text_type, tlang, mode, sentence
+        parse, text_type, tlang, mode, 
+        sentence, cpd_analysis
     )
     
     return res, status, error
     
 
-def evaluation(gold_analysis, pred_analysis):
+def evaluation(gold_analysis, pred_analysis, grain="fine"):
     """ """
     
     rel_type_matches = 0.0
     rel_index_matches = 0.0
     
     complete_rel_matches = 0.0
+
+    no_analysis = 0
+    
+    gold_pred_pairs = []
     
     for j in range(len(gold_analysis) - 1):
         gold_index, gold_word, gold_relation = gold_analysis[j]
@@ -263,19 +398,50 @@ def evaluation(gold_analysis, pred_analysis):
             gold_rel_type, gold_rel_index = gold_relation.split(",")
             pred_rel_type, pred_rel_index = pred_relation.split(",")
             
-#            print(gold_rel_type, gold_rel_index)
-#            print(pred_rel_type, pred_rel_index)
+            if not pred_rel_type or pred_rel_type == "-":
+                no_analysis += 1
+
+            gold_rel_type = gold_rel_type.rstrip("-")
+            
+            gold_rel_type = gold_map.get(gold_rel_type, gold_rel_type)
+            
+            if grain == "coarse":
+                gold_rel_type = fine_to_coarse_dev[gold_rel_type]
+                pred_rel_type = fine_to_coarse_dev.get(pred_rel_type, pred_rel_type)
+            
+#            pred_rel_type = pred_rel_type.replace("_", "-")
             
             if gold_rel_type == pred_rel_type:
                 rel_type_matches += 1
-            elif gold_rel_type == get_code_for_relation(pred_rel_type):
+                gold_pred_pairs.append((gold_rel_type, pred_rel_type))
+            elif gold_rel_type == get_code_for_relation(pred_rel_type) or (grain == "coarse" and gold_rel_type == fine_to_coarse_dev[get_code_for_relation(pred_rel_type)]):
                 rel_type_matches += 1
+                gold_pred_pairs.append((gold_rel_type, fine_to_coarse_dev[get_code_for_relation(pred_rel_type)]))
+            elif gold_rel_type == get_code_for_relation_2(pred_rel_type) or (grain == "coarse" and gold_rel_type == fine_to_coarse_dev[get_code_for_relation_2(pred_rel_type)]):
+                rel_type_matches += 1
+                gold_pred_pairs.append((gold_rel_type, fine_to_coarse_dev[get_code_for_relation_2(pred_rel_type)]))
+            else:
+                gold_pred_pairs.append((gold_rel_type, pred_rel_type))
+            
+#            gold_pred_pairs.append((gold_rel_type, pred_rel_type, get_code_for_relation(pred_rel_type), get_code_for_relation_2(pred_rel_type)))
+            
+            if grain == "coarse":
+                modified_pred_relation_1 = f"{fine_to_coarse_dev[get_code_for_relation(pred_rel_type)]},{pred_rel_index}"
+                modified_pred_relation_2 = f"{fine_to_coarse_dev[get_code_for_relation_2(pred_rel_type)]},{pred_rel_index}"
+            else:
+                modified_pred_relation_1 = f"{get_code_for_relation(pred_rel_type)},{pred_rel_index}"
+                modified_pred_relation_2 = f"{get_code_for_relation_2(pred_rel_type)},{pred_rel_index}"
+            
+            modified_gold_relation = f"{gold_rel_type},{gold_rel_index}"
             
             if gold_rel_index == pred_rel_index:
                 rel_index_matches += 1
             
-            modified_pred_relation = f"{get_code_for_relation(pred_rel_type),pred_rel_index}"
-            if gold_relation == modified_pred_relation:
+            if gold_relation == pred_relation:
+                complete_rel_matches += 1
+            elif modified_gold_relation == modified_pred_relation_1:
+                complete_rel_matches += 1
+            elif modified_gold_relation == modified_pred_relation_2:
                 complete_rel_matches += 1
             
 #            print(rel_type_matches, rel_index_matches, complete_rel_matches)
@@ -291,9 +457,19 @@ def evaluation(gold_analysis, pred_analysis):
     
 #    print("Score: ", rel_type_score, rel_index_score, complete_rel_score)
     
-    return rel_type_score, rel_index_score, complete_rel_score
+    return rel_type_score, rel_index_score, complete_rel_score, no_analysis == 0, gold_pred_pairs
             
+
+def test_sample(sentence, encoding):
+    """ """
     
+    res, status, error = test(sentence, encoding)
+    print(res, status, error)
+    pred_analysis = get_compound_annotation(res)
+#    converted_analysis = handle_xvanxva(pred_analysis)
+    print("\n".join(["\t".join(x) for x in pred_analysis]))
+
+
 def run_all(gold_compounds, encoding):
     """ """
     
@@ -302,40 +478,58 @@ def run_all(gold_compounds, encoding):
     rel_index_scores = []
     complete_rel_scores = []
     
+    results = []
+    gold_pred_pairs = []
+    
     for i in tqdm(range(len(gold_compounds))):
+        print(gold_compounds[i])
+        
         id_, line, gold_analysis = gold_compounds[i]
+
+        compound = line.split(" ")[0]
         
         res, status, error = test(line, encoding)
         
         if error or not status == "Success":
-            errors.append(gold_compounds[i])
-            continue
+            errors.append((id_, compound, gold_analysis))
+            # results.append((id_, compound, gold_analysis, "Failure", error, "", "", ""))
+            # rel_type_scores.append(0.0)
+            # rel_index_scores.append(0.0)
+            # complete_rel_scores.append(0.0)
+            # continue
         
+        print("Text: " + line, "Result: " + res)
         pred_analysis = get_compound_annotation(res)
         converted_analysis = handle_xvanxva(pred_analysis)
         
-#        print(gold_analysis)
-#        print(pred_analysis)
+        gold_analysis_str = json.dumps(gold_analysis)
         
-        rel_type_score, rel_index_score, complete_rel_score = evaluation(gold_analysis, converted_analysis)
+        # print(gold_analysis)
+        # print(converted_analysis)
+        
+        rel_type_score, rel_index_score, complete_rel_score, no_analysis, cur_gold_pred_pairs = evaluation(gold_analysis, converted_analysis)
+        gold_pred_pairs += cur_gold_pred_pairs
+        
+        results.append((id_, compound, gold_analysis, "Success", converted_analysis, str(no_analysis), str(rel_type_score), str(rel_index_score), str(complete_rel_score)))
+        
         rel_type_scores.append(rel_type_score)
         rel_index_scores.append(rel_index_score)
         complete_rel_scores.append(complete_rel_score)
         
-#        print(rel_type_scores, rel_index_scores, complete_rel_scores)
+        print(rel_type_scores, rel_index_scores, complete_rel_scores)
         
     
-#    print(rel_type_scores, rel_index_scores, complete_rel_scores)
+    print(rel_type_scores, rel_index_scores, complete_rel_scores)
     
     avg_rel_type_score = np.mean(rel_type_scores)*100.0
     avg_rel_index_score = np.mean(rel_index_scores)*100.0
     avg_complete_rel_score = np.mean(complete_rel_scores)*100.0
     
-    overall_result = {
+    scores = {
         "avg_rel_type_score" : avg_rel_type_score,
         "avg_rel_index_score" : avg_rel_index_score,
         "avg_complete_rel_score" : avg_complete_rel_score,
     }
     
-    return overall_result, errors
+    return scores, errors, results, gold_pred_pairs
         
--- run_compound_analysis_evaluation.py
+++ run_compound_analysis_evaluation.py
@@ -1,11 +1,13 @@
 import sys
 import os
 
+import json
+
 import devtrans
 
 from evaluate import run_all
 
-script, in_dir = sys.argv
+script, in_dir, out_, err, res, pair = sys.argv
 
 
 def read_contents(name_):
@@ -60,11 +62,11 @@ for f in os.listdir(in_dir):
         
         morph = entry[5]
         
-        relation = entry[7]
+        relation = entry[6]
         
         samAsa_representation = entry[11] if len(entry) >= 12 else ""
         
-        if "-" in word:
+        if "-" in word and "(" not in word:
             compound_str += word
             cur_cmpnt_id += 1
             
@@ -90,12 +92,20 @@ for f in os.listdir(in_dir):
             
             compound_details.append((f"1.{c_index}", word, "कर्ता,2.1", samAsa_representation))
             compound_id += 1
-            if "द्वि" in morph:
-                verb = "स्तः"
-            elif "बहु" in morph:
-                verb = "सन्ति"
+            if any([x in morph for x in ["1", "2"]]):
+                if "द्वि" in morph:
+                    verb = "स्तः"
+                elif "बहु" in morph:
+                    verb = "सन्ति"
+                else:
+                    verb = "अस्ति"
             else:
-                verb = "अस्ति"
+                if "द्वि" in morph:
+                    verb = "भवतः"
+                elif "बहु" in morph:
+                    verb = "भवन्ति"
+                else:
+                    verb = "भवति"
             
             compound_details.append(("2.1", verb, f"अभिहित_कर्ता,1.{c_index}", ""))
             
@@ -114,12 +124,28 @@ for id_, cpd_str, cpd_details in compounds:
     
     test_compounds.append((id_, cpd_str, new_cpd_details))
 
-overall_result, errors = run_all(test_compounds[:], "Unicode")
+overall_result, errors, temp_results, gold_pred_pairs = run_all(test_compounds[:], "Unicode")
 
-print("Result: " + str(overall_result))
-print(f"Errors in {str(len(errors))} examples")
-for id_, cpd, gold_analysis in errors:
-    print(id_, cpd)
-    for index, word, rel in gold_analysis:
-        print(index, word, rel)
+out_lines = []
+for id_, line, gold_analysis, status, pred_analysis, rel_type_score, rel_index_score, complete_rel_score, no_analysis in temp_results:
+    out_lines.append("\t".join((str(id_), line, json.dumps(gold_analysis, ensure_ascii=False), status, json.dumps(pred_analysis, ensure_ascii=False), rel_type_score, rel_index_score, complete_rel_score, no_analysis)))
+
+out_f = open(out_, "w", encoding="utf-8")
+out_f.write("\n".join(out_lines))
+out_f.close()
+
+err_lines = []
+for id_, line, gold_analysis in errors:
+    err_lines.append("\t".join((str(id_), line, json.dumps(gold_analysis, ensure_ascii=False))))
+
+err_f = open(err, "w", encoding="utf-8")
+err_f.write("\n".join(err_lines))
+err_f.close()
+
+pair_f = open(pair, "w", encoding="utf-8")
+pair_f.write("\n".join(["\t".join(x) for x in gold_pred_pairs]))
+pair_f.close()
+
+with open(res, "w", encoding="utf-8") as f:
+    f.write(json.dumps(overall_result, ensure_ascii=False))
 
